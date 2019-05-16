0e2bd2c76a77a048a8cac572feef99033e01a65a
Sara T <sara.moravej@gmail.com>
2019-05-16 11:34:35 -0500

    Musicard
    Created files for each musical card prop

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import "./styles.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>This is Musicard</h1>
-      <h2>May the oldest instruments win!</h2>
-    </div>
-  );
-}
+ReactDOM.render(<App />, document.getElementById("root"));
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
